4f7609bd999c3efbb45063de08c840dfe27e7a3e
cpfarrell <farrell.chris1@gmail.com>
2014-01-12 13:04:03 -0800

    Automate collection

--- Insight/get_rest_info.py
+++ Insight/get_rest_info.py
@@ -6,78 +6,37 @@ from lxml import etree
 from bs4 import BeautifulSoup
 import requests
 
+import redis_database
+
+redis_db = redis_database.RedisDatabase()
+
 s = requests.Session()
 
-rest_urls = [
-'http://www.yelp.com/biz/sunny-blue-santa-monica',
-'http://www.yelp.com/biz/tar-and-roses-santa-monica',
-'http://www.yelp.com/biz/bay-cities-italian-deli-and-bakery-santa-monica',
-'http://www.yelp.com/biz/melisse-santa-monica-2',
-'http://www.yelp.com/biz/the-misfit-restaurant-bar-santa-monica',
-'http://www.yelp.com/biz/fritto-misto-santa-monica',
-'http://www.yelp.com/biz/urth-caffe-santa-monica',
-'http://www.yelp.com/biz/musha-restaurant-santa-monica',
-'http://www.yelp.com/biz/manchego-santa-monica',
-'http://www.yelp.com/biz/blue-stove-santa-monica',
-'http://www.yelp.com/biz/true-food-kitchen-santa-monica',
-'http://www.yelp.com/biz/seasalt-fish-grill-santa-monica',
-'http://www.yelp.com/biz/fathers-office-santa-monica',
-'http://www.yelp.com/biz/upper-west-santa-monica',
-'http://www.yelp.com/biz/umami-burger-santa-monica',
-'http://www.yelp.com/biz/cha-cha-chicken-santa-monica',
-'http://www.yelp.com/biz/mercado-santa-monica',
-'http://www.yelp.com/biz/the-lobster-santa-monica',
-'http://www.yelp.com/biz/superfood-express-santa-monica',
-'http://www.yelp.com/biz/peas-and-carrots-santa-monica',
-'http://www.yelp.com/biz/spazio-caff%C3%A9-santa-monica',
-'http://www.yelp.com/biz/santa-monica-seafood-santa-monica',
-'http://www.yelp.com/biz/milo-and-olive-santa-monica',
-'http://www.yelp.com/biz/la-vecchia-cucina-santa-monica',
-'http://www.yelp.com/biz/warszawa-santa-monica',
-'http://www.yelp.com/biz/the-counter-santa-monica',
-'http://www.yelp.com/biz/rustic-canyon-santa-monica',
-'http://www.yelp.com/biz/library-alehouse-santa-monica',
-'http://www.yelp.com/biz/ushuaia-argentinean-steakhouse-santa-monica',
-'http://www.yelp.com/biz/boa-steakhouse-santa-monica',
-'http://www.yelp.com/biz/stella-barra-pizzeria-santa-monica',
-'http://www.yelp.com/biz/eat-shabu-santa-monica-2',
-'http://www.yelp.com/biz/tacos-punta-cabras-santa-monica',
-'http://www.yelp.com/biz/hillstone-restaurant-santa-monica',
-'http://www.yelp.com/biz/tender-greens-santa-monica',
-'http://www.yelp.com/biz/california-chicken-cafe-santa-monica',
-'http://www.yelp.com/biz/brus-wiffle-a-waffle-joint-santa-monica',
-'http://www.yelp.com/biz/il-ristorante-di-giorgio-baldi-santa-monica',
-'http://www.yelp.com/biz/jiraffe-santa-monica',
-'http://www.yelp.com/biz/joes-pizza-santa-monica',
-'http://www.yelp.com/biz/z-garden-santa-monica',
-'http://www.yelp.com/biz/r-d-kitchen-santa-monica',
-'http://www.yelp.com/biz/pono-burger-santa-monica',
-'http://www.yelp.com/biz/shaka-shack-burgers-santa-monica',
-'http://www.yelp.com/biz/lares-restaurant-santa-monica',
-'http://www.yelp.com/biz/thyme-cafe-and-market-santa-monica',
-'http://www.yelp.com/biz/kafe-k-santa-monica',
-'http://www.yelp.com/biz/capo-santa-monica',
-'http://www.yelp.com/biz/tacos-por-favor-santa-monica',
-'http://www.yelp.com/biz/huckleberry-santa-monica']
-
-for rest_url in rest_urls:
+while redis_db.num_members("restaurant_to_search") > 0:
+#for rest_url in redis_db.get_members("restaurant_to_search"):
+    rest_url = redis_db.random_member("restaurant_to_search")
     print rest_url
-    rest = s.get(rest_url)
+    rest = s.get("http://www.yelp.com" + rest_url)
 #    rest = s.get('http://www.yelp.com/biz/kotoya-ramen-los-angeles-3')
     data_rest = rest.text
-    soup_rest = BeautifulSoup(data_rest)
+    redis_db.add_info(rest_url, {"yelp_page": data_rest})
+    redis_db.add_to_group("restaurant_searched", rest_url)
+    redis_db.remove_member("restaurant_to_search", rest_url)
 
+    soup_rest = BeautifulSoup(data_rest)
     divs_rest = soup_rest.find_all('div')
     menu_url_list = [div for div in divs_rest if div.get('class') and div['class']==["yelp-menu"]]
     if menu_url_list:
         menu_url = menu_url_list[0].a['href']
 
-        time.sleep(random.uniform(2,7))
+        time.sleep(random.uniform(5,10))
         menu = s.get('http://www.yelp.com' + menu_url)
         data_menu = menu.text
+        redis_db.add_info(rest_url,{"yelp_menu": data_menu})
         soup_menu = BeautifulSoup(data_menu)
         print len(str(soup_menu))
-        time.sleep(random.uniform(2,7))
 
     else:
         print 'No menu found'
+
+    time.sleep(random.uniform(5,10))
--- Insight/get_restaurants.py
+++ Insight/get_restaurants.py
@@ -6,20 +6,32 @@ from lxml import etree
 from bs4 import BeautifulSoup
 import requests
 
+import redis_database
+
 s = requests.Session()
 
-for i in range(5):
-    r = s.get('http://www.yelp.com/search?cflt=restaurants&find_loc=santa+monica&start=' + str(10*i))
-#    r = s.get('http://www.yelp.com/search?cflt=restaurants&find_loc=90025&start=' + str(10*i))
-#r = s.get('http://www.yelp.com/biz/kotoya-ramen-los-angeles-3')
-    data = r.text
-    soup = BeautifulSoup(data)
+redis_db = redis_database.RedisDatabase()
 
-    divs = soup.find_all('div')
-    restaurants = [div.span.a['href'] for div in divs if div.get('class') and len(div.get('class'))>2 and div.get('class')[1] == 'natural-search-result']
+pages_per_neighborhood = 20
 
-    for restaurant in restaurants:
-        print "http://www.yelp.com" + restaurant
+for neighborhood in redis_db.get_members("neighborhoods"):
+    print neighborhood
+    print redis_db.num_members(neighborhood)
+    if redis_db.num_members(neighborhood) < 10*pages_per_neighborhood:
+        for i in range(pages_per_neighborhood):
+            r = s.get('http://www.yelp.com/search?cflt=restaurants&find_loc=' + neighborhood + '&start=' + str(10*i))
+
+            data = r.text
+            soup = BeautifulSoup(data)
+            divs = soup.find_all('div')
+            restaurants = [div.span.a['href'] for div in divs if div.get('class') and len(div.get('class'))>2 and div.get('class')[1] == 'natural-search-result']
+
+            for restaurant in restaurants:
+                redis_db.add_to_group(neighborhood, restaurant)
+                print "http://www.yelp.com" + restaurant
+                if not redis_db.key_known(restaurant):
+                    redis_db.add_key(restaurant)
+                    redis_db.add_to_group("restaurant_to_search", restaurant)
         
-    time.sleep(random.uniform(2,7))
+            time.sleep(random.uniform(15,20))
 
