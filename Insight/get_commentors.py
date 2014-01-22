744f3c7564ab84ea2756eee22020e3dde35015b3
cpfarrell <farrell.chris1@gmail.com>
2014-01-22 15:56:01 -0800

    Work on validation

--- Insight/get_commentors.py
+++ Insight/get_commentors.py
@@ -2,43 +2,79 @@ from collections import defaultdict
 
 from lxml import etree
 from bs4 import BeautifulSoup
+import pymongo
+from pymongo import MongoClient
+import pandas
+from sklearn import linear_model
+from sklearn.externals import joblib
 
-import redis_database
 import sql_database
+import helper
 
-redis_db = redis_database.RedisDatabase()
-db = sql_database.DbAccess('YELP', usr='root')
-db.cursor.execute('DROP TABLE IF EXISTS Review;')
-db.cursor.execute('''CREATE TABLE Review (USERID CHAR(80), NorCalRest CHAR(150), NorCalRating FLOAT, NorCalAvgRating FLOAT, 
-                  SoCalRest CHAR(150), SoCalRating FLOAT, SoCalAvgRating FLOAT);''')
+client = MongoClient()
+db_mongo = client.yelp_database
+posts = db_mongo.posts
+rests_info = posts.find()
+
+sql_db = sql_database.DbAccess('YELP', usr='root')
 
 count = 0
 
 socal_users = defaultdict(list)
 norcal_users = defaultdict(list)
 
+def get_restaurant(soup):
+    links = soup.find_all('link')
+    for link in links:
+        href = link['href']
+        if href.find('biz')!=-1:
+            return href[href.find('biz')-1:]
+
+
 def get_reviews(soup):
     lis = soup.find_all("li")
     reviews = [li for li in lis if li.get("class") and li.get("class")[0]=='review']
     return reviews
 
+def build_row(user, norcal, socal):
+
+    rest1 = socal[2]
+    rest2 = norcal[2]
+    price_diff = int(rest1[1]) - int(rest2[1])
+    rating_diff = rest1[2] - rest2[2]
+    meal_same = rest1[3]==rest2[3]
+    table_same = rest1[4]==rest2[4]
+    new_row = {'User': user, 'NorCalRest': norcal[0], 'NorCalRating': norcal[1], 'NorCalAvgRating': rest2[2], 'NorCalZip': rest2[7],
+               'SoCalRest': socal[0], 'SoCalRating': socal[1], 'SoCalAvgRating': rest1[2], 'SoCalZip': rest1[7], 'r1Type': rest1[0],
+               'r2Type': rest2[0], 'PriceDiff': price_diff, 'RatingDiff': rating_diff, 'MealSame': meal_same, 'TableSame': table_same, 'r1Food': rest1[5], 'r2Food': rest2[5]}
+    return new_row
+
 count = 0
-for restaurant in redis_db.get_members("restaurant_searched"):
+
+for rest_info in rests_info:
     count += 1
     if count%100==0:
         print count
 
-    if count>30:
+    if count>10000:
         break
 
-    rest_info = redis_db.get_info(restaurant)
-    nor_cal = False
-    print restaurant
+    if 'yelp_page' not in rest_info:
+        continue
 
-    sql = ('''SELECT RestaurantType, RestaurantsPriceRange2, Rating, GoodForMeal, RestaurantsTableService, Favorites, Latitude
+    page = rest_info['yelp_page']
+    soup = BeautifulSoup(page)
+
+    restaurant = get_restaurant(soup)
+    if not restaurant:
+        continue
+
+    nor_cal = False
+
+    sql = ('''SELECT RestaurantType, RestaurantsPriceRange2, Rating, GoodForMeal, RestaurantsTableService, Favorites, Latitude, Zip
        FROM Restaurant WHERE Site = "''' + restaurant + '''";''')
-    db.cursor.execute(sql)
-    sql_info = db.cursor.fetchone()
+    sql_db.cursor.execute(sql)
+    sql_info = sql_db.cursor.fetchone()
     if not sql_info:
         continue
 
@@ -83,11 +119,25 @@ for restaurant in redis_db.get_members("restaurant_searched"):
         else:
             socal_users[userid].append((restaurant, rating, sql_info))
 
+rows = []
 for user in norcal_users:
     if user in socal_users:
         for norcal_rest in norcal_users[user]:
             for socal_rest in socal_users[user]:
-                print user
-#                Command = 'INSERT INTO Review (USERID, NorCalRest, NorCalRating, NorCalAvgRating, SoCalRest, SoCalRating, SoCalAvgRating) VALUES("%s", "%s", %f, %f, "%s", %f, %f);' % (user, norcal_rest[0], norcal_rest[1], norcal_rest[2], socal_rest[0], socal_rest[1], socal_rest[2])
-#                db.cursor.execute(Command)
-#                db.commit()
+                rows.append(build_row(user, norcal_rest, socal_rest))
+
+df = pandas.DataFrame.from_records(rows)
+df = helper.transform(df)
+X = helper.BuildX(df)
+logistic = joblib.load("data/logit.joblib.pkl")
+df['scores'] = logistic.decision_function(X)
+del df['r1Food']
+del df['r1Type']
+del df['r2Food']
+del df['r2Type']
+sql_db.cursor.execute('DROP TABLE IF EXISTS Review;')
+pandas.io.sql.write_frame(df, 'Review', sql_db.cnx, flavor='mysql')
+
+#Command = 'INSERT INTO Review (USERID, NorCalRest, NorCalRating, NorCalAvgRating, SoCalRest, SoCalRating, SoCalAvgRating) VALUES("%s", "%s", %f, %f, "%s", %f, %f);' % (user, norcal_rest[0], norcal_rest[1], norcal_rest[2], socal_rest[0], socal_rest[1], socal_rest[2])
+#sql_db.cursor.execute(Command)
+#sql_db.commit()
--- Insight/predict_rest.py
+++ Insight/predict_rest.py
@@ -44,11 +44,12 @@ def tf_idf(df, r1Reviews, r2Review):
     df['max_words'] = max_words
     df['similarity'] = similarity
     df = df.sort('similarity', ascending=False).reset_index()
+
     return df
 
 def predict_rest(restaurant, miles, zipcode):
     df = pandas.io.sql.read_frame('''
-     SELECT r1.Name as r1Name, r1.FullName as r1FullName, r1.RestaurantType as r1Type, 
+     SELECT r1.Name as r1Name, r1.FullName as r1FullName, r1.RestaurantType as r1Type, r1.Site as r1Site, 
      r2.RestaurantType as r2Type, ABS(r1.RestaurantsPriceRange2 - r2.RestaurantsPriceRange2) as PriceDiff,
      ABS(r1.Rating - r2.Rating) as RatingDiff, r1.Latitude as Latitude, r1.Longitude as Longitude, r1.GoodForMeal=r2.GoodForMeal as MealSame,
      r1.RestaurantsTableService=r2.RestaurantsTableService as TableSame, r1.Favorites as r1Food, r2.Favorites as r2Food '''
@@ -56,7 +57,8 @@ def predict_rest(restaurant, miles, zipcode):
      '''FROM Restaurant r1 JOIN ZipCodes
      ON DISTANCE(ZipCodes.Latitude, ZipCodes.Longitude, r1.Latitude, r1.Longitude) < ''' + miles + '''
      CROSS JOIN Restaurant r2
-     WHERE r2.FullName = "''' + restaurant + '''" AND ZipCodes.Zip = ''' + zipcode + ''' AND r1.NReviews > 100 ORDER BY r1.FullName;''', db.cnx)
+     WHERE (r2.FullName = "''' + restaurant + '''" OR r2.Site = "''' + restaurant + '''") AND ZipCodes.Zip = ''' + zipcode + ''' AND r1.NReviews > 100;'''
+                                  , db.cnx)
 
     df = helper.transform(df)
 
@@ -65,8 +67,9 @@ def predict_rest(restaurant, miles, zipcode):
     logistic = joblib.load("data/logit.joblib.pkl")
     df['scores'] = logistic.decision_function(X)
     df = df.sort('scores', ascending=False).reset_index()
-    #df = df.ix[range(30),:]
-    df =df.ix[df['scores']>3]
+    df = df.ix[range(30),:]
+    #df=df.ix[df['scores']>4]
+
     #Sort by name to keep aligned
     df = df.sort('r1FullName')
 
@@ -76,19 +79,19 @@ def predict_rest(restaurant, miles, zipcode):
     r1Reviews = db.cursor.fetchall()
     r1Reviews = sorted(r1Reviews)
 
-    db.cursor.execute('SELECT FullName, Review FROM Restaurant WHERE FullName = "' + restaurant + '";')
+    db.cursor.execute('SELECT FullName, Review FROM Restaurant WHERE (FullName = "' + restaurant + '" OR Site = "' + restaurant + '");')
     r2Review = db.cursor.fetchone()
 
     df = tf_idf(df, r1Reviews, r2Review)
+
     restaurants = []
     for i in range(n_restaurants):
-        print df.ix[i, 'max_words']
-        #restaurants.append({i :(df.ix[i, 'r1Name'], (df.ix[i, 'max_words']), df.ix[i, 'Latitude'], df.ix[i, 'Longitude'])})
-        restaurants.append({'Name' :df.ix[i, 'r1Name'], 'Words': df.ix[i, 'max_words'], 'Latitude': df.ix[i, 'Latitude'], 'Longitude': df.ix[i, 'Longitude']})
+        restaurants.append({'Name' :df.ix[i, 'r1Name'], 'Words': df.ix[i, 'max_words'], 'Site': df.ix[i, 'r1Site'], 
+                            'Latitude': df.ix[i, 'Latitude'], 'Longitude': df.ix[i, 'Longitude']})
 
-    print restaurants
+    #print restaurants
     return restaurants
 
 if __name__=='__main__':
-#    predict_rest("Kotoya Ramen 11901 Santa Monica Blvd Los Angeles, CA 90025", "10", "94117")
-    predict_rest("Juquila Restaurant 11619 Santa Monica Blvd Los Angeles, CA 90025", "15", "94117")
+    predict_rest("Kotoya Ramen 11901 Santa Monica Blvd Los Angeles, CA 90025", "10", "94117")
+#    print predict_rest("/biz/tender-greens-hollywood", "5", "95135")
