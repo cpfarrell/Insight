8721b309f5b4b3bb113daa4d9fb1a4304820e128
cpfarrell <farrell.chris1@gmail.com>
2014-01-22 12:30:01 -0800

    Port get_rest_info.py to mongodb

--- Insight/get_rest_info.py
+++ Insight/get_rest_info.py
@@ -5,10 +5,15 @@ import random
 from lxml import etree
 from bs4 import BeautifulSoup
 import requests
+import pymongo
+from pymongo import MongoClient
 
 import redis_database
 
 redis_db = redis_database.RedisDatabase()
+client = MongoClient()
+db = client.yelp_database
+posts = db.posts
 
 s = requests.Session()
 
@@ -17,8 +22,10 @@ while redis_db.num_members("restaurant_to_search") > 0:
     print rest_url
     rest = s.get("http://www.yelp.com" + rest_url)
 
+    rest_info = {'restaurant': rest_url}
     data_rest = rest.text
-    redis_db.add_info(rest_url, {"yelp_page": data_rest})
+
+
     redis_db.add_to_group("restaurant_searched", rest_url)
     redis_db.remove_member("restaurant_to_search", rest_url)
 
@@ -27,22 +34,25 @@ while redis_db.num_members("restaurant_to_search") > 0:
 
     pages_rest = [div for div in divs_rest if div.get("id") and div.get("id")=="rpp-count"]
     n_reviews = 0
-    if len(pages)>0:
+    if len(pages_rest)>0:
         try:
-            n_reviews = int(pages[0].contents[0].split()[-1])
+            n_reviews = int(pages_rest[0].contents[0].split()[-1])
         except:
-            n_reviews = int(pages[0].contents[0].split()[-2])
-    redis_db.add_info(restaurant, {"reviews": n_reviews}) 
+            n_reviews = int(pages_rest[0].contents[0].split()[-2])
 
-    #Grab all the reviews from subsequent pages
+    rest_info['reviews'] = n_reviews
+
+    #Loop through and grab up to four more pages of reviews
     pages = n_reviews/40
     for i in range(1,pages+1):
+        if i > 4:
+            break
         rest = s.get("http://www.yelp.com" + rest_url + "?start=" + str(40*i))
         data_rest = rest.text
-        redis_db.add_info(rest_url, {"yelp_page" + str(i): data_rest})
-        redis_db.add_to_group("restaurant_full_reviews", rest_url)
+        rest_info['yelp_page' + str(i)] = data_rest
         time.sleep(random.uniform(15,20))
 
+    #Grab the menu if possible as well
     menu_url_list = [div for div in divs_rest if div.get('class') and div['class']==["yelp-menu"]]
     if menu_url_list:
         menu_url = menu_url_list[0].a['href']
@@ -50,11 +60,11 @@ while redis_db.num_members("restaurant_to_search") > 0:
         time.sleep(random.uniform(5,10))
         menu = s.get('http://www.yelp.com' + menu_url)
         data_menu = menu.text
-        redis_db.add_info(rest_url,{"yelp_menu": data_menu})
-        soup_menu = BeautifulSoup(data_menu)
-        print len(str(soup_menu))
+        print len(data_menu)
+        rest_info['yelp_menu'] = data_menu
 
     else:
         print 'No menu found'
 
+    posts.insert(rest_info)
     time.sleep(random.uniform(10,15))
