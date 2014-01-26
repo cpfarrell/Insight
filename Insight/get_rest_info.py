ba43f3491cbd5e3c75e31b943eab5f06f5ddd9e4
cpfarrell <farrell.chris1@gmail.com>
2014-01-25 19:43:16 -0800

    Reduce sleep, bug fix

--- Insight/get_rest_info.py
+++ Insight/get_rest_info.py
@@ -24,6 +24,7 @@ while redis_db.num_members("restaurant_to_search") > 0:
 
     rest_info = {'restaurant': rest_url}
     data_rest = rest.text
+    rest_info['yelp_page'] = data_rest
 
     soup_rest = BeautifulSoup(data_rest)
     divs_rest = soup_rest.find_all('div')
@@ -37,7 +38,7 @@ while redis_db.num_members("restaurant_to_search") > 0:
             n_reviews = int(pages_rest[0].contents[0].split()[-2])
 
     rest_info['reviews'] = n_reviews
-
+    print n_reviews
     #Loop through and grab up to four more pages of reviews
     pages = n_reviews/40
     for i in range(1,pages+1):
@@ -46,23 +47,21 @@ while redis_db.num_members("restaurant_to_search") > 0:
         rest = s.get("http://www.yelp.com" + rest_url + "?start=" + str(40*i))
         data_rest = rest.text
         rest_info['yelp_page' + str(i)] = data_rest
-        time.sleep(random.uniform(15,20))
+        time.sleep(random.uniform(5,15))
 
     #Grab the menu if possible as well
     menu_url_list = [div for div in divs_rest if div.get('class') and div['class']==["yelp-menu"]]
     if menu_url_list:
         menu_url = menu_url_list[0].a['href']
 
-        time.sleep(random.uniform(5,10))
+        time.sleep(random.uniform(5,15))
         menu = s.get('http://www.yelp.com' + menu_url)
         data_menu = menu.text
-        print len(data_menu)
         rest_info['yelp_menu'] = data_menu
 
-    else:
-        print 'No menu found'
 
     posts.insert(rest_info)
-    redis_db.add_to_group("restaurant_searched", rest_url)
+    print rest_info.keys()
+    redis_db.add_to_group("restaurant_with_info", rest_url)
     redis_db.remove_member("restaurant_to_search", rest_url)
     time.sleep(random.uniform(10,15))
